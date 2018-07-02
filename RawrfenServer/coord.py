3b39fdd34520faa10fe6c62cab267bab1bc0925f
Astarisk <astarisk@gmail.com>
2018-07-02 03:12:37 -0400

    Added in custom __str__ and __repl__ strings.

---
+++ RawrfenServer/coord.py
@@ -0,0 +1,11 @@
+class Coord:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "({0} {1})".format(self.x, self.y)
+
+    def __repr__(self):
+        return "Coord({0} , {1})".format(self.x, self.y)
